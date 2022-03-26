b156e17117f062f4d6e98fc5149a277b00c18333
vatsalstar <74692621+vatsalstar@users.noreply.github.com>
2022-03-26 10:49:52 +0530

    Add files via upload

--- avg.py
+++ avg.py
@@ -4,5 +4,5 @@ import plotly.figure_factory as pff
 
 df=pd.read_csv("P-108.csv")
 
-fig=pff.create_distplot([df["Avg Rating"].tolist()],["Mobile Brand"],show_hist=False)
+fig=pff.create_distplot([df["Avg Rating"].tolist()],["Mobile Brand"])
 fig.show()
